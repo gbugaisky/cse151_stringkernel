047a6d874c3b8323c7faf0ee8359b371f3bddda5
Bryan Ransil <bryan.ransil@gmail.com>
2013-05-21 15:01:09 -0700

    Added some steps for the algorithm

---
+++ code/KernelPerceptron.py
@@ -0,0 +1,13 @@
+#0. Variables
+# The size of our alphabet
+# The size of the feature map for p = 3
+# The size of the feature map for p = 4
+
+
+#1. As we read from the file:
+'''
+    create a linear classifier 
+    record the vectors for which we had mistakes
+    Return the array of vectors for which we had mistakes
+'''
+#2. 
